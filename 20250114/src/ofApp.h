579f0c6467a57f0e1b6397fe980bff2e6e1682ae
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2025-01-14 18:59:01 +0900

    20250114

---
+++ 20250114/src/ofApp.h
@@ -0,0 +1,14 @@
+// ofApp.h
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
+
+    // Add any other methods or variables here if needed
+};
+
